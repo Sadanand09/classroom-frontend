da738d7d9873bce9887ecc87b1170dd5f88437bc
Sadanand09 <sadanandgurude9@gmail.com>
2026-01-29 20:54:43 +0530

    Initial commit

---
+++ src/pages/subjects/create.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubjectsCreate() {
+  return (
+    <div>
+        Subject create
+    </div>
+  )
+}
+
+export default SubjectsCreate
---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubjectsList() {
+  return (
+    <div>
+      Subject list
+    </div>
+  )
+}
+
+export default SubjectsList
